ff7a25dd2020657c5cd23cb79f98940990f47c5c
boy52hz <tharadon.saenmart@mail.kmutt.ac.th>
2023-11-20 15:25:26 +0700

    refactor: changed API_VERSION env to be a semver with an env validator

--- src/main.ts
+++ src/main.ts
@@ -35,8 +35,8 @@ async function bootstrap() {
   const documentBuilder = new DocumentBuilder()
     .setTitle('WinWin API')
     .setDescription('An application programming interface for Motorcycle Taxi')
-    .addServer(`http://localhost:${PORT}`, 'Local server')
-    .addServer('http://capstone23.sit.kmutt.ac.th/pl1', 'Production server')
+    .addServer(`http://${HOST}:${PORT}`, 'Local server')
+    .addServer('https://capstone23.sit.kmutt.ac.th/pl1', 'Production server')
     .setVersion(API_VERSION)
     .build();
   const document = SwaggerModule.createDocument(app, documentBuilder);
