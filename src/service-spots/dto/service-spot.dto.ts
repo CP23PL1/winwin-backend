a54098dea8f6054b3f48bf6d0b387f7c77390067
boy52hz <tharadon.saenmart@mail.kmutt.ac.th>
2023-11-22 23:16:39 +0700

    refactor: added api property to ServiceSpotDto

--- src/service-spots/dto/service-spot.dto.ts
+++ src/service-spots/dto/service-spot.dto.ts
@@ -1,12 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Point } from 'typeorm';
 
 export class ServiceSpotDto {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   placeId: string;
+  @ApiProperty({
+    example: {
+      type: 'Point',
+      coordinates: [123.123, 13.123],
+    },
+  })
   coords: Point;
+  @ApiProperty()
   approved: boolean;
+  @ApiProperty()
   createdAt: string;
+  @ApiProperty()
   updatedAt: string;
+  @ApiProperty()
   distance: number;
 }
