bb5b23a1b172c7a6d3c99b7ae5306d98b5fe2bec
Siripong Phiwkhaw <72536609+siripongphiwkhaw@users.noreply.github.com>
2024-02-12 18:28:11 +0700

    fix: update migration DeleteDriverTable

--- src/db/migrations/1707298729560-DeleteDriverTable.ts
+++ src/db/migrations/1707298729560-DeleteDriverTable.ts
@@ -2,6 +2,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class DeleteDriverTable1707298729560 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    // Drop Constraint
+    await queryRunner.query(
+      `ALTER TABLE "service_spot" DROP CONSTRAINT "FK_8559f83d04890fc46edd3685ed2"`,
+    );
     await queryRunner.query(`DROP TABLE IF EXISTS "driver"`);
   }
 
