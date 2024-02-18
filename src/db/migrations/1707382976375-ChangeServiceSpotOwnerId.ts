1d8db3475f45eb11fb95931ab3677ffad229b8ea
boy52hz <tharadon.saenmart@mail.kmutt.ac.th>
2024-02-19 04:31:49 +0700

    feat: added user resource

--- src/db/migrations/1707382976375-ChangeServiceSpotOwnerId.ts
+++ src/db/migrations/1707382976375-ChangeServiceSpotOwnerId.ts
@@ -8,21 +8,9 @@ export class ChangeServiceSpotOwnerId1707382976375 implements MigrationInterface
       `ALTER TABLE "service_spot" RENAME COLUMN "serviceSpotOwnerUid" TO "serviceSpotOwnerId"`,
     );
     await queryRunner.query(`ALTER TABLE "service_spot" ALTER COLUMN "coords" TYPE geometry`);
-    await queryRunner.query(`ALTER TABLE "service_spot" DROP COLUMN "serviceSpotOwnerId"`);
-    await queryRunner.query(`ALTER TABLE "service_spot" ADD "serviceSpotOwnerId" integer NOT NULL`);
-    await queryRunner.query(
-      `ALTER TABLE "service_spot" ADD CONSTRAINT "UQ_05b4bd09b8a0d5bf669c644d12f" UNIQUE ("serviceSpotOwnerId")`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "service_spot" DROP CONSTRAINT "UQ_05b4bd09b8a0d5bf669c644d12f"`,
-    );
-    await queryRunner.query(`ALTER TABLE "service_spot" DROP COLUMN "serviceSpotOwnerId"`);
-    await queryRunner.query(
-      `ALTER TABLE "service_spot" ADD "serviceSpotOwnerId" character varying NOT NULL`,
-    );
     await queryRunner.query(
       `ALTER TABLE "service_spot" ALTER COLUMN "coords" TYPE geometry(GEOMETRY,0)`,
     );
